c47fafb1322027aa76e503443c2a219288b1a345
IvanSanchezDev <ivancampus2023@gmail.com>
2023-09-01 08:42:50 -0500

    feat: :sparkles: config conexion a la bd

---
+++ src/configDatabase/connection.js
@@ -0,0 +1,31 @@
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@clusterauthenticacion.lobajpy.mongodb.net/`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export async function connect () {
+  try {
+    await client.connect()
+    const database = client.db('#nombreDatabase')
+    return database
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function closeConnection () {
+  try {
+    await client.close()
+    console.log('Database connection closed')
+  } catch (error) {
+    console.error('Error closing the database connection')
+    console.error(error)
+  }
+}
