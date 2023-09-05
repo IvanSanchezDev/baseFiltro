74a7bfcaf5138db05a0093680e80db04c2d7dfd2
IvanSanchezDev <ivancampus2023@gmail.com>
2023-09-05 11:37:16 -0500

    refactor: :ambulance: Cambios de nombre de archivos y eliminacion archivos innecesarios

--- src/routes/router.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import routesVersioning from 'express-routes-versioning'
-export const routes = Router()
-
-const version = routesVersioning()
-
-// EJEMPLO DE IMPLEMENTACION
-routes.get('/', [passport.authenticate('bearer', { session: false })], version({ '1.0.0': metodo }))
